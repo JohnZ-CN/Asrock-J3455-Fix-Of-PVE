6eaec18677e32a8026a26f3f873858ca512cba6e
JohnZ-CN <104251289+JohnZ-CN@users.noreply.github.com>
2023-06-15 18:33:48 +0800

    Update 1-Install-build-dependencies.sh

--- 1-Install-build-dependencies.sh
+++ 1-Install-build-dependencies.sh
@@ -1,2 +1,1 @@
-Install build dependencies
 apt install asciidoc-base automake bc bison cpio debhelper dh-python file flex gcc git kmod libdw-dev libelf-dev libiberty-dev libnuma-dev libpve-common-perl libslang2-dev libssl-dev libtool lintian lz4 perl-modules python-is-python3 python3-dev rsync sed sphinx-common tar xmlto zlib1g-dev
