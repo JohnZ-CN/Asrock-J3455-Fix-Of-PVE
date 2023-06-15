923093f9ca31a81bb1e36b88205754af04120330
JohnZ-CN <104251289+JohnZ-CN@users.noreply.github.com>
2023-06-15 19:37:31 +0800

    Update 4-Initialize-the-submodules.sh

--- 4-Initialize-the-submodules.sh
+++ 4-Initialize-the-submodules.sh
@@ -1,3 +1,10 @@
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install devscripts -y && mk-build-deps --install
+
+#自动安装依赖
+yes| mk-build-deps --install
+#自动安装依赖之后，卸载deps包
+yes| mk-build-deps --install --remove
+#基本所有包，都可以通过make deb来进行编译，如果出现依赖问题，可以通过apt安装对应的依赖。
+
 cd pve-kernel && make submodule
