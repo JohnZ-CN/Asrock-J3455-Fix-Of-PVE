af90557ad5599037b3f80bd80ee4560d2e8ee312
JohnZ-CN <104251289+JohnZ-CN@users.noreply.github.com>
2023-06-15 19:36:57 +0800

    Update 4-Initialize-the-submodules.sh

--- 4-Initialize-the-submodules.sh
+++ 4-Initialize-the-submodules.sh
@@ -1,2 +1,3 @@
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get install devscripts -y && mk-build-deps --install
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install devscripts -y && mk-build-deps --install
 cd pve-kernel && make submodule
