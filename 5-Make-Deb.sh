4481d0a632a8d4c9de49e49fcfbb81dc089ceddf
JohnZ-CN <104251289+JohnZ-CN@users.noreply.github.com>
2023-06-16 10:04:42 +0800

    Update 5-Make-Deb.sh

--- 5-Make-Deb.sh
+++ 5-Make-Deb.sh
@@ -1,5 +1,3 @@
-#git clone -b pve-kernel-5.15 git://git.proxmox.com/git/pve-kernel.git pve-kernel
-git clone https://git.proxmox.com/git/pve-kernel.git
 cd pve-kernel
 #安装依赖
 mk-build-deps --install
